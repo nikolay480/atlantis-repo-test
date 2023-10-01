e9cf45be22128bb625d89966e88431aa7c1900d0
Nikolay Chaykovskiy <nickrer1988@gmail.com>
2023-10-01 15:56:47 +0500

    delete files

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "access_key" {
-  value = yandex_iam_service_account_static_access_key.storage-key.access_key
-  sensitive = true
-}
-
-output "secret_key" {
-  value = yandex_iam_service_account_static_access_key.storage-key.secret_key
-  sensitive = true
-}
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-#=========== main ==============
-cloud_id  = "b1gso2vle70451bc3egm"
-folder_id = "b1ga4u00jltahn3l6fp9"
